55393c52cacc4df588bf6e8a5063f80da10e2295
Frozenclaw <Swansonbenjamin434@yahoo.com>
2018-01-17 15:59:14 -0600

    Finished

---
+++ output/lua/SiegeSetupImprovements/BalanceMisc.lua
@@ -0,0 +1,10 @@
+
+kArmorySupply = 1 -- Was 5
+kMacSupply = 5 -- Was 10
+kRoboticsFactorySupply = 0 -- Was 5
+kInitialMACs = 2 -- Was 0
+kMACConstructEfficacy = .5 -- Was .3
+kEchoRange = 10 -- Was 8 
+kUpgradeGestationTime = .5 -- Was 2
+kNumMines = 5 -- Was 2
+kBellySlideCost = 10 -- Was 20
--- output/lua/SiegeSetupImprovements/FileHooks.lua
+++ output/lua/SiegeSetupImprovements/FileHooks.lua
@@ -1,2 +1,3 @@
 ModLoader.SetupFileHook( "lua/Balance.lua", "lua/SiegeSetupImprovements/Balance.lua" , "post" )
 ModLoader.SetupFileHook( "lua/ConstructMixin.lua", "lua/SiegeSetupImprovements/ConstructMixin.lua" , "post" )
+ModLoader.SetupFileHook( "lua/BalanceMisc.lua", "lua/SiegeSetupImprovements/BalanceMisc.lua" , "post" )
