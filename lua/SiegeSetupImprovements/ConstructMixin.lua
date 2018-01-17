7ba59a59298749bba96f1c7c7265f16479d0eeb0
Frozenclaw <Swansonbenjamin434@yahoo.com>
2018-01-17 12:02:20 -0600

    Update ConstructMixin.lua

--- lua/SiegeSetupImprovements/ConstructMixin.lua
+++ lua/SiegeSetupImprovements/ConstructMixin.lua
@@ -6,22 +6,30 @@ function ConstructMixin:OnConstructUpdate(deltaTime)
         
     local effectTimeout = Shared.GetTime() - self.timeLastConstruct > 0.65
     self.underConstruction = not self:GetIsBuilt() and not effectTimeout
-    
+	
     -- Only Alien structures auto build.
     -- Update build fraction every tick to be smooth.
     if not self:GetIsBuilt() and GetIsAlienUnit(self) and GetIsMarineUnit(self) then
-
+	
+	for index, constructable in ipairs(GetEntitiesWithMixin("Construct")) do
+        
+		if not constructable:GetIsBuilt() then
+			constructable:SetConstructionComplete()
+		end
+            
+		end
+		
         if not self.GetCanAutoBuild or self:GetCanAutoBuild() then
         
-           	local multiplier = self.hasDrifterEnzyme and kDrifterBuildRate or kAutoBuildRate
-            	multiplier = multiplier * ( (HasMixin(self, "Catalyst") and self:GetIsCatalysted()) and kNutrientMistAutobuildMultiplier or 1 or ( (HasMixin(self, "PowerConsumer") and self:GetIsPowered()) ))
-           	self:Construct(deltaTime * multiplier)
+            local multiplier = self.hasDrifterEnzyme and kDrifterBuildRate or kAutoBuildRate
+            multiplier = multiplier * ( (HasMixin(self, "Catalyst") and self:GetIsCatalysted()) and kNutrientMistAutobuildMultiplier or 1 )
+            self:Construct(deltaTime * multiplier)
+			
+		
         end
 		
-    end
-    
+    end  
     
 end
 
-
-end
+end -- end server 
