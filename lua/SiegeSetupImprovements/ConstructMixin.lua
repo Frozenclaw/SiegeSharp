c7a4f769f145a6222316dc90ea2e5508a27901c7
Frozenclaw <Swansonbenjamin434@yahoo.com>
2018-01-17 14:35:38 -0600

    Update ConstructMixin.lua

--- lua/SiegeSetupImprovements/ConstructMixin.lua
+++ lua/SiegeSetupImprovements/ConstructMixin.lua
@@ -4,51 +4,51 @@ if Server then
   
 function ConstructMixin:OnConstructUpdate(deltaTime)
         
-    local effectTimeout = Shared.GetTime() - self.timeLastConstruct > 0.65
-    self.underConstruction = not self:GetIsBuilt() and not effectTimeout
+    	local effectTimeout = Shared.GetTime() - self.timeLastConstruct > 0.65
+   	self.underConstruction = not self:GetIsBuilt() and not effectTimeout
     
-    -- Only Alien structures auto build.
-    -- Update build fraction every tick to be smooth.
-    if not self:GetIsBuilt() and GetIsAlienUnit(self) then
+   	-- Only Alien structures auto build.
+    	-- Update build fraction every tick to be smooth.
+   	if not self:GetIsBuilt() and GetIsAlienUnit(self) then
 
-        if not self.GetCanAutoBuild or self:GetCanAutoBuild() then
+        	if not self.GetCanAutoBuild or self:GetCanAutoBuild() then
         
-            local multiplier = self.hasDrifterEnzyme and kDrifterBuildRate or kAutoBuildRate
-            multiplier = multiplier * ( (HasMixin(self, "Catalyst") and self:GetIsCatalysted()) and kNutrientMistAutobuildMultiplier or 1 )
-            self:Construct(deltaTime * multiplier)
+            		local multiplier = self.hasDrifterEnzyme and kDrifterBuildRate or kAutoBuildRate
+            		multiplier = multiplier * ( (HasMixin(self, "Catalyst") and self:GetIsCatalysted()) and kNutrientMistAutobuildMultiplier or 1 )
+            		self:Construct(deltaTime * multiplier)
             
-        end
+        	end
     
-    end
+   	 end
 	
 	if not self:GetIsBuilt() and GetIsMarineUnit(self) then
 		self:SetConstructionComplete()
 	end 
     
-    if self.timeDrifterConstructEnds then
+   	if self.timeDrifterConstructEnds then
         
-        if self.timeDrifterConstructEnds <= Shared.GetTime() then
+       	 	if self.timeDrifterConstructEnds <= Shared.GetTime() then
         
-            self.hasDrifterEnzyme = false
-            self.timeDrifterConstructEnds = nil
+            		self.hasDrifterEnzyme = false
+            		self.timeDrifterConstructEnds = nil
             
-        end
+        	end
         
-    end
+   	end
 
-    -- respect the cheat here; sometimes the cheat breaks due to things relying on it NOT being built until after a frame
-    if GetGamerules():GetAutobuild() then
-        self:SetConstructionComplete()
+    	-- respect the cheat here; sometimes the cheat breaks due to things relying on it NOT being built until after a frame
+    	if GetGamerules():GetAutobuild() then
+        	self:SetConstructionComplete()
 	
 
-    end
+    	end
     
-    if self.underConstruction or not self.constructionComplete then
-        return kUpdateIntervalFull
-    end
+    	if self.underConstruction or not self.constructionComplete then
+        	return kUpdateIntervalFull
+    	end
     
-    -- stop running once we are fully constructed
-    return false
+    	-- stop running once we are fully constructed
+    	return false
     
 end
 
